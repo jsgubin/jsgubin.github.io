ef75bc9cd39af73141be84e87192e5d54bf37eef
jsgubin <jsgubin@gmail.com>
2022-05-22 15:08:24 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python2 jemdoc.py group.jemdoc
+python2 jemdoc.py hire.jemdoc
+python2 jemdoc.py activities.jemdoc
+python2 jemdoc.py index.jemdoc
+python2 jemdoc.py courses.jemdoc
+python2 jemdoc.py publications.jemdoc
+python2 jemdoc.py research.jemdoc
